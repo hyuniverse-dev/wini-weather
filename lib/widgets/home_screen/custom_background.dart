b622c4f4f660fc5ebe74a14e8ee3c6efacb3e42f
박현 <ggolre90@gmail.com>
2024-02-27 10:53:11 +0900

    Refactor: Remove custom_background.dart code

--- lib/widgets/home_screen/custom_background.dart
+++ lib/widgets/home_screen/custom_background.dart
@@ -1,50 +1,5 @@
 import 'package:flutter/material.dart';
 
-Widget daySunnyBackground() {
-  return Center(
-    child: Image.asset(
-      'assets/images/backgrounds/day_sunny_background.png',
-      fit: BoxFit.cover,
-    ),
-  );
-}
-
-Widget nightSunnyBackground() {
-  return Center(
-    child: Image.asset(
-      'assets/images/backgrounds/night_sunny_background.png',
-      fit: BoxFit.cover,
-    ),
-  );
-}
-
-Widget dayCloudBackground() {
-  return Center(
-    child: Image.asset(
-      'assets/images/backgrounds/day_cloud_background.png',
-      fit: BoxFit.cover,
-    ),
-  );
-}
-
-Widget nightCloudBackground() {
-  return Center(
-    child: Image.asset(
-      'assets/images/backgrounds/night_cloud_background.png',
-      fit: BoxFit.cover,
-    ),
-  );
-}
-
-Widget dayMistBackground() {
-  return Center(
-    child: Image.asset(
-      'assets/images/backgrounds/day_mist_background.png',
-      fit: BoxFit.cover,
-    ),
-  );
-}
-
 Widget getBackgroundImage({required String status}) {
   return Center(
     child: Image.asset(
