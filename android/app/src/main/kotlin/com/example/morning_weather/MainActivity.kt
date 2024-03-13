eeb38cfa1915744632305e4e8121a685d1905f6c
박현 <hyun@mncf.io>
2024-03-14 08:22:41 +0900

    Chore: MainActivity.kt

--- android/app/src/main/kotlin/com/example/morning_weather/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/morning_weather/MainActivity.kt
@@ -6,18 +6,20 @@ import android.os.Bundle
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import com.google.android.gms.ads.MobileAds
 
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MobileAds.initialize(this) {}
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
-                "Schedule Notification", // channelId
-                "Schedule Notification", // channelName, 여기서는 `main.dart`에 명시된 값을 사용했습니다.
+                "Schedule Notification",
+                "Schedule Notification",
                 NotificationManager.IMPORTANCE_DEFAULT
             ).apply {
-                description = "Information about weather" // channelDescription
+                description = "Information about weather"
             }
             val notificationManager: NotificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
